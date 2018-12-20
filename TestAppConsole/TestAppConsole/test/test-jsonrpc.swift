638f6e9fe8f334777470c185a7fd650698f90602
WooKyoung Noh <wookay.noh@gmail.com>
2018-12-21 07:53:46 +0900

    test jsonrpc

--- TestAppConsole/TestAppConsole/test/test-jsonrpc.swift
+++ TestAppConsole/TestAppConsole/test/test-jsonrpc.swift
@@ -6,26 +6,37 @@ import JSONRPCKit
 public struct CastError: Exception {
 }
 
+// https://github.com/bricklife/JSONRPCKit
 struct Subtract: JSONRPCKit.Request {
-    typealias Response = Int
-    
+    typealias Response = String
+
     let minuend: Int
     let subtrahend: Int
-    
+
+    // JSONRPC: var method: String { get }
     var method: String {
         return "subtract"
     }
-    
+
+    // JSONRPC: var parameters: Any? { get }
     var parameters: Any? {
         return [minuend, subtrahend]
     }
-    
+
+    // JSONRPC: func response(from resultObject: Any) throws -> Response
     func response(from resultObject: Any) throws -> Response {
         if let response = resultObject as? Response {
             return response
         } else {
             throw CastError()
         }
+    }
+}
+
+extension Batch1 {
+    public func get(_ object: Any) -> Batch1.Responses {
+        let response = try! self.responses(from: object)
+        return response
     }
 }
 
@@ -36,6 +47,10 @@ class TestJSONRPC: WTestCase {
         let batchFactory = BatchFactory(version: "2.0", idGenerator: NumberIdGenerator())
         let batch = batchFactory.create(request1)
         Assert.equal(batch.requestObject as! [String: Any], ["id": 1, "params": [42, 23], "jsonrpc": "2.0", "method": "subtract"])
+
+        let responseObject: Any = ["jsonrpc": "2.0", "result": "abc", "id": 1]
+        let got = batch.get(responseObject)
+        Assert.equal(got, "abc")
     }
 
 }
