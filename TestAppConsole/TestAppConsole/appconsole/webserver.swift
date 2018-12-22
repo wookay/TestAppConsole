ca8c522967c7e2f0518611bb741fb1df4a5f730b
WooKyoung Noh <wookay.noh@gmail.com>
2018-12-23 04:14:57 +0900

    render JSON

--- TestAppConsole/TestAppConsole/appconsole/webserver.swift
+++ TestAppConsole/TestAppConsole/appconsole/webserver.swift
@@ -12,6 +12,7 @@ import Embassy // SelectorEventLoop KqueueSelector DefaultHTTPServer
 struct Render {
     var status: Int
     var body: Data
+    var content_type: String
 }
 
 protocol MIME {
@@ -23,42 +24,86 @@ struct Text: MIME {
 struct HTML: MIME {
 }
 
-func render(_ typ: Any.Type, _ status: Int, _ value: String) -> Render {
-    let data = Data(string: value)
-    return Render(status: status, body: data)
+struct JSON: MIME {
+}
+
+func content_type(_ typ: Any.Type) -> String {
+    switch typ {
+    case is Text.Type:
+        return "text/plain; charset=utf-8"
+    case is HTML.Type:
+        return "text/html; charset=utf-8"
+    case is JSON.Type:
+        return "application/json; charset=utf-8"
+    default:
+        break
+    }
+    return ""
+}
+
+func render<T: Encodable>(_ status: Int, _ typ: Any.Type, _ value: T) -> Render {
+    var data: Data
+    switch typ {
+    case is Text.Type, is HTML.Type:
+        data = Data(string: value as? String)
+    case is JSON.Type:
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        do {
+            data = try encoder.encode(value)
+        } catch {
+            switch value {
+            case is String:
+                let quot = "\""
+                data = Data(string: string(quot, value, quot))
+            default:
+                data = Data(string: string(value))
+            }
+        }
+    default:
+        data = Data()
+    }
+    return Render(status: status, body: data, content_type: content_type(typ))
 }
 
 class WebServer {
     var loop: SelectorEventLoop
     var server: DefaultHTTPServer
     
+    
     public init(port: Int, handle: @escaping ((String, [String: Any]) -> Render)) {
         let loop = try! SelectorEventLoop(selector: try! KqueueSelector())
         let server = DefaultHTTPServer(eventLoop: loop, interface: "127.0.0.1", port: port) {
         (
-        env: [String: Any],
-        startResponse: ((String, [(String, String)]) -> Void),
-        sendBody: ((Data) -> Void)
+            env: [String: Any],
+            startResponse: ((String, [(String, String)]) -> Void),
+            sendBody: ((Data) -> Void)
         ) in
-        let path = something(env["PATH_INFO"], "")
-        let resp = handle(path, env)
-        startResponse(string(resp.status, " OK"), [])
-        let EOF = Data()
-        sendBody(resp.body)
-        sendBody(EOF)
+            let path = something(env["PATH_INFO"], "")
+            let resp = handle(path, env)
+            startResponse(string(resp.status, " OK"), [
+                ("Content-Type", resp.content_type),
+                ("Content-Length", string(length(resp.body))),
+            ])
+            let EOF = Data()
+            sendBody(resp.body)
+            sendBody(EOF)
         }
         self.loop = loop
         self.server = server
     }
     
-    public func start() {
+    public func start(forever: Bool = false) {
         do {
             try self.server.start()
         } catch {
         }
-        // self.loop.runForever()
-        DispatchQueue(label: "server").async {
+        if forever {
             self.loop.runForever()
+        } else {
+            DispatchQueue(label: "server").async {
+                self.loop.runForever()
+            }
         }
     }
     
--- TestAppConsole/TestAppConsole/test/test-embassy.swift
+++ TestAppConsole/TestAppConsole/test/test-embassy.swift
@@ -9,25 +9,25 @@ class TestEmbassy: WTestCase {
     func handle(path: String, env: [String: Any]) -> Render {
         switch path {
         case "/":
-            return render(Text.self, 200, "Hello")
+            return render(200, JSON.self, "Hello")
         default:
             break
         }
-        return render(Text.self, 404, "Not Found")
+        return render(404, HTML.self, "<p>Not Found</p>")
     }
 
     @objc func test_embassy() {
         let web = WebServer(port: 8081, handle: self.handle)
-        web.start()
+        web.start() //forever: true)
 
         var test_cnt = 0
         http_get(url: "http://127.0.0.1:8081") { (data, resp) in
-            Assert.equal(data, Data(string: "Hello"))
+            Assert.equal(data, Data(string: "\"Hello\""))
             Assert.equal(resp.statusCode, 200)
             test_cnt += 1
         }
         http_get(url: "http://127.0.0.1:8081/none") { (data, resp) in
-            Assert.equal(data, Data(string: "Not Found"))
+            Assert.equal(data, Data(string: "<p>Not Found</p>"))
             Assert.equal(resp.statusCode, 404)
             test_cnt += 1
         }
