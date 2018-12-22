4381d0d7129f5babd10cdfec03a935501516fb1c
WooKyoung Noh <wookay.noh@gmail.com>
2018-12-23 01:10:53 +0900

    render

--- TestAppConsole/TestAppConsole/test/test-embassy.swift
+++ TestAppConsole/TestAppConsole/test/test-embassy.swift
@@ -4,32 +4,61 @@ import Foundation
 import MuckoSwift
 import Embassy // SelectorEventLoop KqueueSelector DefaultHTTPServer
 
+struct Render {
+    var status: Int
+    var body: Data
+}
+
+func render(_ typ: Any.Type, _ status: Int, _ value: String) -> Render {
+    let data = Data(string: value)
+    return Render(status: status, body: data)
+}
+
 class TestEmbassy: WTestCase {
-    private func makeWebServer(_ f: @escaping () -> Data) -> (SelectorEventLoop, DefaultHTTPServer) {
+    private func makeWebServer(_ f: @escaping ([String: Any]) -> Render) -> (SelectorEventLoop, DefaultHTTPServer) {
         let loop = try! SelectorEventLoop(selector: try! KqueueSelector())
         let server = DefaultHTTPServer(eventLoop: loop, interface: "127.0.0.1", port: 8081) {
-            (env: [String: Any], startResponse: ((String, [(String, String)]) -> Void), sendBody: ((Data) -> Void)) in
-            startResponse("200 OK", [])
+            (
+                env: [String: Any],
+                startResponse: ((String, [(String, String)]) -> Void),
+                sendBody: ((Data) -> Void)
+            ) in
+            let r = f(env)
+            startResponse(string(r.status, " OK"), [])
             let EOF = Data()
-            sendBody(f())
+            sendBody(r.body)
             sendBody(EOF)
         }
         return (loop, server)
     }
 
-    func http_get(_ urlstr: String, _ f: @escaping (Data?) -> Void) {
+    func http_get(_ urlstr: String, _ f: @escaping (Data?, HTTPURLResponse) -> Void) {
         let url = URL(url: urlstr)
         let completion: (Data?, URLResponse?, Error?) -> Void = { data, response, error in
-            f(data)
+            let response = response as! HTTPURLResponse
+            f(data, response)
         }
         URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
     }
 
-    @objc func test_embassy() {
-        let (loop, server) = makeWebServer {
-            let data = Data(string: "Hello")
-            return data
+    typealias Text = String
+    func route(env: [String: Any]) -> Render {
+        if let path = env["PATH_INFO"] as? String {
+            switch path {
+            case "/":
+                return render(Text.self, 200, "Hello")
+            default:
+                break
+            }
         }
+        return render(Text.self, 404, "Not Found")
+    }
+
+    @objc func test_embassy() {
+        let (loop, server) = makeWebServer { env in
+            self.route(env: env)
+        }
+    
         do {
             try server.start()
         } catch {
@@ -40,12 +69,23 @@ class TestEmbassy: WTestCase {
             loop.runForever()
         }
 
-        http_get("http://127.0.0.1:8081") { data in
-            Assert.equal("Hello", String(data: data))
+        var test_cnt = 0
+        http_get("http://127.0.0.1:8081") { (data, response) in
+            Assert.equal(data, Data(string: "Hello"))
+            Assert.equal(response.statusCode, 200)
+            test_cnt += 1
         }
-
+        http_get("http://127.0.0.1:8081/none") { (data, response) in
+            Assert.equal(data, Data(string: "Not Found"))
+            Assert.equal(response.statusCode, 404)
+            test_cnt += 1
+        }
+        
         usleep(useconds_t(0.3 * 1000_000)) // n seconds
         server.stopAndWait()
         loop.stop()
+        
+        Assert.equal(test_cnt, 2)
     }
+
 }
